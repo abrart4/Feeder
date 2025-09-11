e5e023934b4235b2a328bcfd42ed3f33af82294a
AbrarTaseen <abrart4@nycstudents.net>
2025-09-11 11:39:20 -0400

    Funny business

--- Feeder.java
+++ Feeder.java
@@ -18,13 +18,10 @@ public class Feeder {
      * feeder, as described in part (a) Precondition: numBirds > 0
      */
     public void simulateOneDay(int numBirds) {
-        if (Math.random() < 0.05) currentFood = 0;
-        else {
-            int eaten = (int) (Math.random() * 41) + 10;
-            eaten *= numBirds;
-            currentFood -= eaten;
-            if (currentFood < 0) currentFood = 0;
-        }
+        int eaten = (int) (Math.random() * 41) + 10;
+        eaten *= numBirds;
+        currentFood -= eaten;
+        if (currentFood < 0) currentFood = 0;
     }
 
     /**
